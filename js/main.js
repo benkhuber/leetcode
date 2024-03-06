10d5a8cd3ebc025aab2fc537b20425a282a9c8b9
Ben Huber <benkhuber@gmail.com>
2024-03-05 20:23:10 -0800

    67 add binary, 217 contains duplicates

--- js/main.js
+++ js/main.js
@@ -1,44 +1,7 @@
-var addBinary = function(a, b) {
-  binA = 0;
-  binB = 0;
-  sum = 0;
-  output = ""
+var containsDuplicate = function(nums) {
+  const newNums = [...new Set(nums)]
   
-  for (let i = 0; i < a.length; i++)
-  {
-    if (a[i] == "1")
-    {
-      binA += (2 ** (a.length - 1 - i));
-    }
-  }
-
-  for (let i = 0; i < b.length; i++)
-  {
-    if (b[i] == "1")
-    {
-      binB += (2 ** (b.length - 1 - i));
-    }
-  }
-
-  sum = binA + binB;
-
-  while (sum > 0)
-  {
-    if (sum % 2 == 1)
-    {
-      output += "1";
-    }
-    else
-    {
-      output += "0";
-    }
-    sum = Math.floor(sum / 2);
-  }
-
-  let reverseOutput = output.split('').reverse().join('');
-
-  return reverseOutput;
+  return !(newNums.length == nums.length)
 };
 
-console.log(addBinary("11", "1"));
-// console.log(addBinary("1010", "1011"));
+console.log(containsDuplicate([1,2,3]))
